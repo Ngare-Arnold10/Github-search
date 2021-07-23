81a0f3b2631c33e3e31a06f6d46b9b7a7b1beb19
reagan2020 <reaganteflon@.com>
2021-07-23 20:06:07 +0300

    add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
