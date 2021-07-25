ce31f7c6a62e1934e412e64fe50513b233fbd8a4
reagan2020 <reaganteflon@.com>
2021-07-25 09:39:31 +0300

    update changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, RouterLinkActive } from '@angular/router';
+import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "form",
+    component: FormComponent
+  },
+  {
+    path: "display",
+    component: DisplayComponent
+  },
+  {
+    path: '',
+    redirectTo: "/form", pathMatch: "full"
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
