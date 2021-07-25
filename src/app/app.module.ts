5cfcb34a609f203229f0f7486bba1a51edbbc906
reagan2020 <reaganteflon@.com>
2021-07-25 10:02:30 +0300

    update changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DisplayComponent } from './display/display.component';
 import { FormComponent } from './form/form.component';
+import { DisplayComponent } from './display/display.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ColorDirective } from './color.directive';
+// import { NgProgressModule } from '@ngx-progressbar/core';
+// import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TimeCountPipe } from './time-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent,
     FormComponent,
-    NotFoundComponent
+    DisplayComponent,
+    ColorDirective,
+    NotFoundComponent,
+    TimeCountPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // NgProgressModule.forRoot(),
+    // NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
